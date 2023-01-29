bf35e61de4c53c3b4c6b331a9d076b364c04f96e
osawa-koki <github@osawa.dev>
2023-01-29 20:13:27 +0900

    イロイロ修正。

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,12 @@ export default function HelloWorld() {
     <Layout>
       <div id="About" className="mt-3">
         <h1>Here, About page.</h1>
-        <p>Next.js大好きです。<br />本当に使いやすいです。<br />僕はTypeScript × Next.jsを強くオススメします。<br /><br />静的型付け言語って難しいと思われがちですが、エディタの機能が豊富な現在は型付け言語の方が簡単に書くことができます。<br />さらには字句解析と構文解析に加えて、コンパイル時に、コンパイラによる意味解析を行ってくれるため、動的型付け言語と比較して動作に信頼性があります。<br /><br />また、Reactと比べてNext.jsはSSGによってビルド時にHTMLが生成されるため、SPA(SSR)と比べて高速に動作し、ユーザビリティが高いです。</p>
+        <ul>
+          <li>'Terraform'でAWS上にリソースを構築</li>
+          <li>'S3'に静的コンテンツを配置</li>
+          <li>'GH Actions'でCI/CDを実現</li>
+        </ul>
+        モダンなフロントエンド開発環境を構築するためのテンプレートです。<br />もっとも、静的ページであればGH Pagesでも十分ですが、、、
       </div>
     </Layout>
   );
